46cd63af179930b7e4e09b9ff013e1144c179a88
dorudia <dorudia@gmail.com>
2021-05-15 15:30:22 +0300

    add contemt

---
+++ app.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+    document.querySelector('.navbar').classList.toggle('active')
+    hamburger.classList.toggle('active')
+})
